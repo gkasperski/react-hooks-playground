94fa7328e25d58cf3af906e3501faff0f9a3ce9f
Grzegorz Kasperski <grzegorz.kasperski@globallogic.com>
2019-01-04 16:45:03 +0100

    Refactor App.hooks to use hooks state

--- src/components/App.hooks.js
+++ src/components/App.hooks.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 import ResourceList from "./ResourceList.hooks";
+import { POSSIBLE_STATES } from "../resources.service";
 
-const POSSIBLE_STATES = {
-  POSTS: 'POSTS',
-  TODOS: 'TODOS',
-};
-
-class App extends Component {
-  state = { resource: POSSIBLE_STATES.POSTS };
-
-
-  render() {
-    const { resource } = this.state;
-    return (
+const App = () => {
+  const [resource, setResource] = useState(POSSIBLE_STATES.POSTS);
+  
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.setState({ resource: POSSIBLE_STATES.POSTS})}>Posts</button>
-          <button onClick={() => this.setState({ resource: POSSIBLE_STATES.TODOS})}>Todos</button>
-        </div>
-        <ResourceList resource={resource}></ResourceList>
+        <button onClick={() => setResource(POSSIBLE_STATES.POSTS)}>
+          Posts
+        </button>
+        <button onClick={() => setResource(POSSIBLE_STATES.TODOS)}>
+          Todos
+        </button>
       </div>
-    );
-  }
-}
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
 
 export default App;
--- src/resources.service.js
+++ src/resources.service.js
@@ -2,8 +2,11 @@ import { useState, useEffect } from "react";
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
+export const POSSIBLE_STATES = {
+  POSTS: "POSTS",
+  TODOS: "TODOS"
+};
 export const getResource = resourceName => fetch(`${API_URL}/${resourceName}`).then(data => data.json()).catch(err => console.err("INVALID RESOURCE"));
-
 export const useResources = resource => {
   const [ resources, setResources ] = useState([]);
   useEffect(() => {(() => getResource(resource).then(data => setResources(data)))()}, [resource]);
